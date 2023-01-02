b65bbe089646cda7df3d00079b8cbab24889ed5f
Roy Jumah <royjumah17@gmail.com>
2023-01-02 17:25:57 +0300

    Add function call to Index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import './reset.css';
-import * as Elements from './modules/Dom.js';
+import "./reset.css";
+import * as Elements from "./modules/Dom.js";
+import viewMeal from "./modules/Functions.js";
+import getMeal from "./modules/Api.js";
 
-Elements.popupBtn.addEventListener('click', () => {
+getMeal(url).then((data) => viewMeal(data));
+
+Elements.popupBtn.addEventListener("click", () => {
   const openModal = () => {
-    Elements.modal.style.display = 'block';
+    Elements.modal.style.display = "block";
   };
   openModal();
 });
 
-Elements.closeBtn.addEventListener('click', () => {
+Elements.closeBtn.addEventListener("click", () => {
   const closeModal = () => {
-    Elements.modal.style.display = 'none';
+    Elements.modal.style.display = "none";
   };
   closeModal();
 });
