a8fd6f598995810362763c60e093fc308152c8a3
Roy Jumah <royjumah17@gmail.com>
2023-01-02 17:22:52 +0300

    Add fetch API

---
+++ src/Function.js
@@ -0,0 +1,39 @@
+import { modal } from "./Dom.js";
+
+function viewMeal(data) {
+  data.forEach((element) => {
+    modal.innerHTML = `
+    <div class="popup-content">
+        <div class ="popup-image">
+            <span class ="popup-food">
+                <img src="${element.strCategoryThumb}" alt="Pizza">
+            </span>
+            <span class="close-window">&times;</span>
+        </div>
+        <div class="meal-title">
+            <h2>${element.strCategory}</h2> 
+        </div>
+        <div class="description">
+            <p>${element.strCategoryDescription}</p>
+        </div>
+        <div class="comment-section">
+            <h2 class="comment-title">Comments()</h2>
+            <div class="display-comments">
+                <ul>
+                    <li>REally nice</li>
+                    <li>Very yummy</li>
+                </ul>
+            </div>
+        </div>
+        <div class="add-comments">
+            <h2 class="add-comm">Add a comment</h2>
+            <form class="form">
+                <input type="text" class="name" value="Your name">
+                <input type="text" class="insights" value="Your insights">
+                <button type="submit" class="submit"> Comment</button>
+            </form>
+        </div>    
+    </div>`;
+  });
+}
+export default viewMeal;
