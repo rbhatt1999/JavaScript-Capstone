75e990c096e957a282cc3680b7f9f159de3a39bc
Roy Jumah <royjumah17@gmail.com>
2023-01-02 16:46:40 +0300

    Add DOM elements

---
+++ src/dom.js
@@ -0,0 +1,5 @@
+const modal = document.querySelector(".popup-window");
+const popupBtn = document.querySelector(".open-popup");
+const closeBtn = document.querySelector(".close-window");
+
+export { modal, popupBtn, closeBtn };
