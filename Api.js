780edd32edd9806a43372e3b564b170d31d82342
Roy Jumah <royjumah17@gmail.com>
2023-01-02 17:24:22 +0300

    Add API calls

---
+++ Api.js
@@ -0,0 +1,9 @@
+url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+const getMeal = async (url) => {
+  const request = await fetch(url);
+  const data = await request.json();
+  return data.categories;
+};
+
+export default getMeal;
