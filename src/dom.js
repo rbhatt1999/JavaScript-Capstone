289c71241bad9c4a93bdf3501e5eb2cfc4584d4d
rbhatt199924 <rbhatt199924@gmail.com>
2023-01-02 21:22:57 +0530

    fix linter issue

--- src/dom.js
+++
@@ -1,4 +0,0 @@
-const modal = document.querySelector('.popup-container');
-const comment = document.querySelectorAll('.comment');
-const Cards = document.querySelector('.cards');
-export { modal, comment, Cards };
