ab797f9e9f61870991cf38e6f4735cf74d534cb4
Roy Jumah <royjumah17@gmail.com>
2023-01-02 17:00:46 +0300

    Fix linting erros

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import "./reset.css";
-import * as Elements from "./modules/dom.js";
+import './reset.css';
+import * as Elements from './modules/Dom.js';
 
-Elements.popupBtn.addEventListener("click", (e) => {
+Elements.popupBtn.addEventListener('click', () => {
   const openModal = () => {
-    Elements.modal.style.display = "block";
+    Elements.modal.style.display = 'block';
   };
   openModal();
 });
 
-Elements.closeBtn.addEventListener("click", (e) => {
+Elements.closeBtn.addEventListener('click', () => {
   const closeModal = () => {
-    Elements.modal.style.display = "none";
+    Elements.modal.style.display = 'none';
   };
   closeModal();
 });
