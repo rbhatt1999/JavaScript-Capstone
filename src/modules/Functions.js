995a7821ba237008963e5e344b30769efb80d891
RoyJumah <royjumah17@gmail.com>
2023-01-03 11:04:28 +0300

    Add comment display to window

--- src/modules/Functions.js
+++ src/modules/Functions.js
@@ -1,4 +1,4 @@
-import { ProductsCounter, modal, Cards } from './Dom.js';
+import { ProductsCounter, modal, Cards } from "./Dom.js";
 
 function DisplayPopup(data, examples) {
   modal.innerHTML = ` <div class="closeBtn">X</div>
@@ -35,20 +35,17 @@ function DisplayPopup(data, examples) {
                      <hr/>
                     <div class="comment-list">
                         <ul class="comments">
-                           <li class="single-comment">
-                               <img class="user" src="https://www.w3schools.com/howto/img_avatar.png" alt="user" >
-                               <b class="user-comment">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus consectetur architecto quidem molestiae deserunt vero voluptas odit asperiores nemo laborum cum laudantium eaque voluptate esse nobis voluptatem iusto, est maxime!</b>
-                           </li>             
+                                        
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
    </div>`;
-  const images = document.querySelector('.images');
+  const images = document.querySelector(".images");
   examples.meals.slice(0, 3).forEach((element) => {
-    const div = document.createElement('div');
-    div.classList.add('single-product-example');
+    const div = document.createElement("div");
+    div.classList.add("single-product-example");
     div.innerHTML = ` 
     <div class="img-popholdpop">
         <div class="imgex">
@@ -61,10 +58,10 @@ function DisplayPopup(data, examples) {
     images.appendChild(div);
   });
 
-  const closeBtn = document.querySelector('.closeBtn');
+  const closeBtn = document.querySelector(".closeBtn");
 
-  closeBtn.addEventListener('click', () => {
-    modal.classList.toggle('hide');
+  closeBtn.addEventListener("click", () => {
+    modal.classList.toggle("hide");
   });
 }
 
@@ -83,18 +80,18 @@ function DisplayCards(data) {
               <h3>${element.strCategory}</h3>
               <div class="interactions">
               <div><i id="${
-  element.idCategory
-}" class="fa-solid fa-comment fa-lg"></i>  355  </div>       <div><i class="fa-solid fa-heart fa-lg"></i> <b id="${
-  element.idCategory
-}" class="likes-counter">355</b> </div>
+                element.idCategory
+              }" class="fa-solid fa-comment fa-lg"></i>  355  </div>       <div><i class="fa-solid fa-heart fa-lg"></i> <b id="${
+      element.idCategory
+    }" class="likes-counter">355</b> </div>
               </div>
               <div>
               ${element.strCategoryDescription.substr(0, 50)}...
               </div>
               
           <button id="${
-  element.idCategory
-}" class="button comment"><i class="fa-solid fa-comments"></i> Comments</button>
+            element.idCategory
+          }" class="button comment"><i class="fa-solid fa-comments"></i> Comments</button>
           </div>
   
       </div>
