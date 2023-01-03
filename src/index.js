ca57607d8a1515944072f6dc9fb2950522bb6cc1
RoyJumah <royjumah17@gmail.com>
2023-01-03 07:28:27 +0300

    Add a new comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import './reset.css';
-import Api from './modules/Api.js';
+import "./reset.css";
+import Api from "./modules/Api.js";
+import { modal } from "./modules/Dom.js";
 
 const ApiCalls = new Api();
 ApiCalls.GetMeals();
+
+UpdateComment = async (comments) => {
+  const userComments = await this.AddComment(comments);
+  return { userComments };
+};
+
+modal.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = modal.names.value.trim();
+  const comment = form.insights.value.trim();
+  const comments = {
+    username: username,
+    comment: comment,
+    item_id: `${index}`,
+  };
+  console.log(comments);
+});
